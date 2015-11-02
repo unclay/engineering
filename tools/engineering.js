92114288c34986fdf262c5854ffc76b4d552989c
wangchenglin <wangchenglin@claypot.local>
2015-11-02 23:19:02 +0800

    修改sass，使之支持多文件配置

--- tools/engineering.js
+++ tools/engineering.js
@@ -57,26 +57,51 @@ module.exports = function(gulp){
 		var confUrl = path.join( process.env.INIT_CWD, 'package.json' );
 		// 项目配置文件内容
 		var conf = !!fs.readFileSync( confUrl, 'utf-8' ) ? JSON.parse(fs.readFileSync( confUrl, 'utf-8' )) : {};
-		// sass源文件 如: *.scss
-		var from = !!conf.sass && !!conf.sass.from && !!conf.sass.from.match(/\.scss$/gi) ? conf.sass.from : 'style.scss';
-		// sass目标文件 如: *.css
-		var to = !!conf.sass && !!conf.sass.to && !!conf.sass.to.match(/\.css$/gi) ? conf.sass.to : from.replace('.scss', '.css');
-		// sass编译风格
-		if( !!conf.sass && !!conf.sass.outputStyle ){
-			if( typeof conf.sass.outputStyle === 'number' ){
-				outputStyle = outputStyleTemplate[ conf.sass.outputStyle ] || outputStyleTemplate[0];
-			} else if( !!conf.sass.outputStyle.match( new RegExp('^' + outputStyleTemplate.join('|') + '$', 'gi') ) ){
-				outputStyle = conf.sass.outputStyle;
+		// 设置默认编译配置
+		var list = [{
+			'from': path.join(cssDirs, 'style.scss'),
+			'to': 'style.css',
+			'outputStyle': outputStyleTemplate[0]
+		}];
+		// 对外部编译风格进行校正处理
+		var getOutputStyle = function(outputStyle){
+			if( typeof outputStyle === 'number' ){
+				outputStyle = outputStyleTemplate[ outputStyle ] || outputStyleTemplate[0];
+			} else if( typeof outputStyle === 'string' && !!outputStyle.match( new RegExp('^' + outputStyleTemplate.join('|') + '$', 'gi') ) ){
+				outputStyle = outputStyle;
 			} else {
-				console.warn('[warn] outputStyle value is not a number nor a string!');
+				console.warn('[warn] outputStyle("'+ outputStyle +'") value must be a number or string!');
+				outputStyle = outputStyleTemplate[0];
+			}
+			return outputStyle;
+		}
+		// 提取正确的编译配置
+		if( !!conf.sass && conf.sass.length > 0 ){
+			for( var i=0; i<conf.sass.length; i++ ){
+				if( !!conf.sass[i].from && !!conf.sass[i].to && !!conf.sass[i].outputStyle ){
+					if( conf.sass[i].from.match(/\.scss$/gi) && conf.sass[i].to.match(/\.css$/gi) ){
+						if( i == 0 ) list = []; // 有正确的编译风格时，清除默认配置
+						list.push({
+							'from': path.join(process.env.INIT_CWD, conf.sass[i].from),
+							'to': conf.sass[i].to,
+							'outputStyle': getOutputStyle( conf.sass[i].outputStyle )
+						});
+					} else {
+						// 编译源文件和目标文件必须是scss和css
+						console.error('from: ("' + conf.sass[i].from + '") must be *.scss, to: ("' + conf.sass[i].to + '") must be *.css');
+					}
+				}
 			}
 		}
-		gulp.src(path.join(cssDirs, from))
-			.pipe(sass({
-                outputStyle: outputStyle
-            }).on("error", sass.logError))
-            .pipe(rename(to))
-			.pipe(gulp.dest(cssDirs));
+		// 多文件编译
+		for( var i=0; i<list.length; i++ ){
+			gulp.src( list[i].from )
+				.pipe(sass({
+	                outputStyle: list[i].outputStyle
+	            }).on("error", sass.logError))
+	            .pipe(rename(list[i].to))
+				.pipe(gulp.dest(cssDirs));
+		}
 	});
 
 	gulp.task('build', function(){
